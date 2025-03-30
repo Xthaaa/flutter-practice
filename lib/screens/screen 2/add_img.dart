9a8a11a239758e964580afc7186f99a4695be8fd
Astha Anjali <iamanjaliastha@gmail.com>
2025-03-30 22:33:33 +0530

    added one tap pause and like gesture

--- lib/screens/screen 2/add_img.dart
+++ lib/screens/screen 2/add_img.dart
@@ -5,19 +5,11 @@ class AddImg extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox.expand(
-      // child: Align(
-      //   alignment: Alignment.bottomLeft,
-      //   child: Padding(
-      //     padding: EdgeInsets.only(left: 18, bottom: 10),
-      //     child: SizedBox(
-            child: Image.asset(
-              'assets/image/lady2.png',
-              width: 450,
-              height: 200,
-            ),
-          );
-    //     ),
-    //   ),
-    // );
+      child: Image.asset(
+        'assets/image/lady2.png',
+        width: 450,
+        height: 200,
+      ),
+    );
   }
 }
